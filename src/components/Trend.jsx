02fb1c63936d712b277a2cb0d4804c6f351dd9c2
LeCram0ni <marcel.stacha@gmail.com>
2026-08-20 22:49:18 +0200

    ui fix

--- src/components/Trend.jsx
+++ src/components/Trend.jsx
@@ -6,7 +6,7 @@ export default function Trend({ selectedCurrency1, selectedCurrency2, first, lat
    let trendWording = trend > 0 ? <span className="rising">gestiegen</span> : <span className="dropping">gefallen</span>
 
    return (<>
-      <div className="top-card">
+      <div className="top-card" id="trend">
          <h5>Trend</h5>
          <div>
             <span className="bold">{selectedCurrency1}</span> <span>{"=>"}</span> <span className="bold">{selectedCurrency2}</span>
@@ -15,7 +15,7 @@ export default function Trend({ selectedCurrency1, selectedCurrency2, first, lat
             <br />
             <span className="bold">{Math.abs(trend).toFixed(4)}%</span>
          </div>
-         <h6 className="sub-info">letzte {OFFSET} Tage</h6>
       </div>
+      <h6 className="sub-info">letzte {OFFSET} Tage</h6>
    </>)
 }
