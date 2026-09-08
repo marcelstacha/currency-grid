efb82572007b1194721a6b41ef848602b9329668
LeCram0ni <marcel.stacha@gmail.com>
2026-09-08 22:44:20 +0200

    animation fix

--- src/components/CurrencySwitch.jsx
+++ src/components/CurrencySwitch.jsx
@@ -11,7 +11,7 @@ function Bill({ path, classes }) {
                style={{ gridArea: "1 / 1 / 2 / 2" }}
                initial={{ opacity: 0, scale: 1 }}
                animate={{ opacity: 1, scale: 1 }}
-               exit={{ opacity: 0, scale: -0.5, rotate: 9 }}
+               exit={{ opacity: 0, scale: 1, rotate: 0 }}
                transition={{ duration: 0.4, ease: "easeInOut" }}
             />
          </AnimatePresence>
