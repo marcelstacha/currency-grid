f477639cf67d69b792a59f97f0c6b61447bb9723
LeCram0ni <marcel.stacha@gmail.com>
2026-09-08 22:36:48 +0200

    glowing bills animation

--- src/components/CurrencySwitch.jsx
+++ src/components/CurrencySwitch.jsx
@@ -1,30 +1,55 @@
+import { motion, AnimatePresence } from "motion/react";
+function Bill({ path, classes }) {
+   return (
+      <div style={{ display: "grid", placeItems: "center" }}>
+         <AnimatePresence>
+            <motion.img
+               key={path}
+               src={path}
+               className={`bill ${classes}`}
+               alt=""
+               style={{ gridArea: "1 / 1 / 2 / 2" }}
+               initial={{ opacity: 0, scale: 1 }}
+               animate={{ opacity: 1, scale: 1 }}
+               exit={{ opacity: 0, scale: -0.5, rotate: 9 }}
+               transition={{ duration: 0.4, ease: "easeInOut" }}
+            />
+         </AnimatePresence>
+      </div>
+   );
+}
+
 export default function CurrencySwitch({ selectedCurrency1, selectedCurrency2, onClick, isIcon = false }) {
 
-   const path1 = selectedCurrency1.toLowerCase() + ".jpg"
-   const path2 = selectedCurrency2.toLowerCase() + ".jpg"
+   const path1 = selectedCurrency1.toLowerCase() + ".jpg";
+   const path2 = selectedCurrency2.toLowerCase() + ".jpg";
 
    return (
-
-      isIcon ?
-         <div className="switch-icon" >
+      isIcon ? (
+         <button className="switch-icon darkmode-btn" aria-label="Währungen tauschen" onClick={onClick}>
             {selectedCurrency1 == selectedCurrency2 ? "?" : ""}
-         </div>
-         :
-         <>
-            <div className="bill-container" onClick={onClick}>
-               <div className="blurred-images">
-                  <img className="bill blurred" title="bill-blurred-1" src={path1} />
-                  <img className="bill blurred" title="bill-blurred-2" src={path2} />
-               </div>
-               <div className="blurred-images-bg">
-                  <img className="bill blurred" title="bill-blurred-1" src={path1} />
-                  <img className="bill blurred" title="bill-blurred-2" src={path2} />
-               </div>
-               <div className="foreground-images">
-                  <img className="shadow bill" title="bill-1" src={path1} />
-                  <img className="shadow bill" title="bill-2" src={path2} />
+         </button>
+      ) : (
+         <button className="bill-container darkmode-btn" onClick={onClick} aria-label="Währungen tauschen">
+
+            <div className="blurred-images">
+               <Bill path={path1} classes="bill-1 blurred" />
+               <Bill path={path2} classes="bill-2 blurred" />
+            </div>
+
+            <div className="blurred-images-bg">
+               <div className="rotate">
+                  <Bill path={path1} classes="bill-1 blurred" />
+                  <Bill path={path2} classes="bill-2 blurred" />
                </div>
             </div>
-         </>
-   )
+
+            <div className="foreground-images">
+               <Bill path={path1} classes="shadow bill-1" />
+               <Bill path={path2} classes="shadow bill-2" />
+            </div>
+
+         </button>
+      )
+   );
 }
